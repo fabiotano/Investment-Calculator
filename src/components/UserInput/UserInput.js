ab3c88fa9832d3cb8d5cbec99dc11e91cfe33707
fabiotano <fabio.capriotti20@gmail.com>
2023-10-03 15:40:42 -0300

    Handling Events

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function UserInput() {
 
+  const inputChangeHandler = (input, value) => {
+    //it will handle the inputs all at once
+    console.log(input)
+    console.log(value);;
+  }
+
+  const resetHandler = (event) => {
+    // console.log('RESET');
+    //..
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault()
+    // console.log('SUMBIT');
+    //..
+  }
+
   return (
-    <form className="form">
+    <form className="form" onSubmit={submitHandler}>
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" />
+          <input type="number" id="current-savings" onChange={ (event) => inputChangeHandler('current-savings', event.target.value ) }/>
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" />
+          <input type="number" id="yearly-contribution" onChange={ (event) => inputChangeHandler('yearly-contribution', event.target.value ) }/>
         </p>
       </div>
       <div className="input-group">
@@ -19,16 +36,16 @@ export default function UserInput() {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" />
+          <input type="number" id="expected-return" onChange={ (event) => inputChangeHandler('expected-return', event.target.value ) }/>
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" />
+          <input type="number" id="duration" onChange={ (event) => inputChangeHandler('duration', event.target.value ) }/>
         </p>
       </div>
 
       <p className="actions">
-        <button type="reset" className="buttonAlt">
+        <button type="reset" className="buttonAlt" onClick={resetHandler}>
           Reset
         </button>
         <button type="submit" className="button">
