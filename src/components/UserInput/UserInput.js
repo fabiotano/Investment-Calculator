2c8d3fed56a3c1392f7a82a55f0a4654f8c4e75b
fabiotano <fabio.capriotti20@gmail.com>
2023-10-03 22:43:01 -0300

    Managing States

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,18 +1,38 @@
 import React, {useState} from "react";
 
+const initialCurrentSavings = 10000
+const initialContribution = 1200
+const initialExpectedReturn = 7
+const initialDuration = 10
+
 export default function UserInput() {
 
+  const [currentSavings, setCurrentSavings] = useState(initialCurrentSavings)
+  const [yearlyContribution, setYearlyContribution] = useState(initialContribution)
+  const [expectedReturn, setExpectedReturn] = useState(initialExpectedReturn)
+  const [duration, setDuration] = useState(initialDuration)
+
   const inputChangeHandler = (input, value) => {
-    //it will handle the inputs all at once
-    console.log(input)
-    console.log(value);;
-  }
+
+    if (input === 'currentSavings') {
+      setCurrentSavings(value);
+    } else if (input === 'yearlyContribution') {
+      setYearlyContribution(value);
+    } else if (input === 'expectedReturn') {
+      setExpectedReturn(value);
+    } else if (input === 'duration') {
+      setDuration(value);
+    }
+  };
 
   const resetHandler = (event) => {
     // console.log('RESET');
-    //..
+    setCurrentSavings(initialCurrentSavings)
+    setYearlyContribution(initialContribution)
+    setExpectedReturn(initialExpectedReturn)
+    setDuration(initialDuration)
   }
-
+  
   const submitHandler = (event) => {
     event.preventDefault()
     // console.log('SUMBIT');
@@ -24,11 +44,11 @@ export default function UserInput() {
       <div className="input-group">
         <p>
           <label htmlFor="current-savings">Current Savings ($)</label>
-          <input type="number" id="current-savings" onChange={ (event) => inputChangeHandler('current-savings', event.target.value ) }/>
+          <input type="number" id="current-savings" value={currentSavings} onChange={ (event) => inputChangeHandler('currentSavings', event.target.value ) }/>
         </p>
         <p>
           <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-          <input type="number" id="yearly-contribution" onChange={ (event) => inputChangeHandler('yearly-contribution', event.target.value ) }/>
+          <input type="number" id="yearly-contribution" value={yearlyContribution} onChange={ (event) => inputChangeHandler('yearlyContribution', event.target.value ) }/>
         </p>
       </div>
       <div className="input-group">
@@ -36,11 +56,11 @@ export default function UserInput() {
           <label htmlFor="expected-return">
             Expected Interest (%, per year)
           </label>
-          <input type="number" id="expected-return" onChange={ (event) => inputChangeHandler('expected-return', event.target.value ) }/>
+          <input type="number" id="expected-return" value={expectedReturn} onChange={ (event) => inputChangeHandler('expectedReturn', event.target.value ) }/>
         </p>
         <p>
           <label htmlFor="duration">Investment Duration (years)</label>
-          <input type="number" id="duration" onChange={ (event) => inputChangeHandler('duration', event.target.value ) }/>
+          <input type="number" id="duration" value={duration} onChange={ (event) => inputChangeHandler('duration', event.target.value ) }/>
         </p>
       </div>
 
